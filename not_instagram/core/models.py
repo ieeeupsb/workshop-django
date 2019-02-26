69660a80f88aa142d126ccc26e21cdd26c5f0b55
Diogo Silva <diogo98s@hotmail.com>
2019-02-26 17:46:18 +0000

    Added models

--- not_instagram/core/models.py
+++ not_instagram/core/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.CharField(max_length=50, default=None, null=True)
+    picture = models.ImageField(upload_to='profile/', default=None, null=True)
+
+class Publication(models.Model):
+    content = models.ImageField(upload_to='photos/')
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    caption = models.CharField(max_length=100, null=True, blank=True)
+    published_at = models.DateTimeField(auto_now_add=True)
+
+class Comment(models.Model):
+    content = models.CharField(max_length=500)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    publication = models.ForeignKey(Publication, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
